d2f5a88829e686419a4fdbf6d4e9faec7fe9fff2
Sasha <sashacarney23@gmail.com>
2018-11-29 18:10:58 -0600

    Itemized table added

--- ORC_app/db/migrate/20181129024315_add_userlist.rb
+++ ORC_app/db/migrate/20181129024315_add_userlist.rb
@@ -5,10 +5,10 @@ class AddUserlist < ActiveRecord::Migration
     users = CSV.read(Dir.pwd + "/db/New_Fake_Users.csv", :headers => true)
     users.each do |row|
      User.create({
-      :first_name => row[0],
-      :last_name => row[1],
-      :email_address => row[2],
-      :student_ID => row[3], 
+      :First_Name => row[0],
+      :Last_Name => row[1],
+      :Email_Address => row[2],
+      :Student_ID => row[3],
       :phone => row[4]
       })
    end
