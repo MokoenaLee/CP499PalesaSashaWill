b61074052aa301ffd8cd733e017eecb94d852ae9
MokoenaLee <palesa.mokoena@coloradocollege.edu>
2018-11-30 09:28:19 -0700

    changes made to stylesheet for table

--- ORC_app/db/schema.rb
+++ ORC_app/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20181130004644) do
-
+ActiveRecord::Schema.define(version: 20181130005726) do
 
   create_table "generals", force: :cascade do |t|
     t.string "type_name"
