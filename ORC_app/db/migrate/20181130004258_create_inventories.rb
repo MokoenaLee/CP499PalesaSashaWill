e57ccb4c958a9bbb97c16be8672fa3615506e663
Sasha <sashacarney23@gmail.com>
2018-12-04 11:36:52 -0600

    Initial id testing + milestone tag

--- ORC_app/db/migrate/20181130004258_create_inventories.rb
+++ ORC_app/db/migrate/20181130004258_create_inventories.rb
@@ -2,6 +2,7 @@ class CreateInventories < ActiveRecord::Migration
   def change
     create_table :inventories do |t|
        t.belongs_to :user,index: true
+       self.primary_key = IID
        t.boolean 'Bulk'
        t.string 'Gear_Type'
        t.string 'Brand'
