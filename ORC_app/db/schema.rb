11559ad675f4dc4b391d7c4c14a3a690770eb5eb
Will Stern <wlbs523@gmail.com>
2018-12-04 12:10:49 -0700

    deleted unnessasary method

--- ORC_app/db/schema.rb
+++ ORC_app/db/schema.rb
@@ -14,12 +14,6 @@
 ActiveRecord::Schema.define(version: 20181203202725) do
 
   create_table "bulks", force: :cascade do |t|
-    t.string  "Gear_Type"
-    t.string  "Gear_Category"
-    t.integer "Quantity"
-    t.string  "location"
-    t.string  "Notes"
-    t.boolean "Bulk"
   end
 
   create_table "generals", force: :cascade do |t|
@@ -38,7 +32,6 @@ ActiveRecord::Schema.define(version: 20181203202725) do
   end
 
   create_table "inventories", force: :cascade do |t|
-    t.integer "user_id"
     t.boolean "Bulk"
     t.string  "Gear_Type"
     t.string  "Brand"
@@ -57,19 +50,30 @@ ActiveRecord::Schema.define(version: 20181203202725) do
     t.string  "Available"
   end
 
-  add_index "inventories", ["user_id"], name: "index_inventories_on_user_id"
-
   create_table "rentals", force: :cascade do |t|
     t.integer  "user_ID"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "Gear_Type"
+    t.string   "item_ID"
     t.string   "rental_date"
     t.string   "return_date"
     t.integer  "days_used"
     t.string   "on_time_price"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "sleepings", force: :cascade do |t|
+    t.string "type_name"
+    t.string "staff"
+    t.string "date"
+    t.string "upstairs"
+    t.string "trip_room"
+    t.string "nso_room"
+    t.string "rented"
+    t.string "in_repair"
+    t.string "total"
+    t.string "track_easy"
+    t.string "comparison"
+    t.string "notes"
   end
 
   create_table "users", force: :cascade do |t|
