af4875a5028c934113bb22bff951bd7f59c71ca3
Will Stern <wlbs523@gmail.com>
2018-12-05 14:21:35 -0700

    Added a Modal to the users page
    hopefully when fully implemented, this modal will replace the show button that causes the whole screen to reload

--- ORC_app/app/assets/javascripts/application.js
+++ ORC_app/app/assets/javascripts/application.js
@@ -1,6 +1,3 @@
-//= require jquery3
-//= require popper
-//= require bootstrap// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -19,3 +16,4 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require modal_js.js
